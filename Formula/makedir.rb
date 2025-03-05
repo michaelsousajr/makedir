eef3a54eeafc1f55d396cbb9d6238e46b7fe5c9f
Michael Sousa <mikey@Michael-Macbook.local>
2025-03-05 15:38:03 -0500

    update

--- Formula/makedir.rb
+++ Formula/makedir.rb
@@ -1,9 +1,12 @@
 class Makedir < Formula
   desc "Better mkdir: Create directories with predefined setups"
   homepage "https://github.com/michaelsousajr/makedir"
-  url "https://github.com/yourusername/makedir/releases/download/v0.1.0/makedir-mac.tar.gz"
-  sha256 "15d9a1faaf993a3c25189b3060dd7d9d7dccd928e74b38344b6f4a7e87e1086d"
   license "MIT"
+
+  on_macos do
+    url "https://github.com/michaelsousajr/makedir/releases/download/v0.1.0/makedir-mac.tar.gz"
+    sha256 "a1ac4847e5af4cd4d68f9db4dcfb475bb75f6821abdb1b80adf73c15b50209a6"
+  end
 
   def install
     bin.install "makedir"
